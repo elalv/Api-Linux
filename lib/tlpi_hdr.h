f31098fa867a1c4fa0088a50b553405598f568ae
elalv <admin@elalv.xyz>
2019-01-08 11:04:49 +0300

    [3.1][UPDATE] Other defines

--- lib/tlpi_hdr.h
+++ lib/tlpi_hdr.h
@@ -37,4 +37,34 @@ typedef enum
 #define min(m, n) ((m) < (n) ? (m) : (n))
 #define max(m, n) ((m) > (n) ? (m) : (n))
 
+/* Some systems don't define 'socklen_t' */
+#if defined(__sgi)
+typedef int socklen_t;
+#endif
+
+#if defined(__sun)
+#include <sys/file.h> /* Has definition of FASYNC */
+#endif
+
+#if !defined(O_ASYNC) && defined(FASYNC)
+/* Some systems define FASYNC instead of O_ASYNC */
+#define O_ASYNC FASYNC
+#endif
+
+#if defined(MAP_ANON) && !defined(MAP_ANONYMOUS)
+/* BSD derivatives usually have MAP_ANON, not MAP_ANONYMOUS */
+#define MAP_ANONYMOUS MAP_ANON
+#endif
+
+#if !defined(O_SYNC) && defined(O_FSYNC)
+/* Some implementations have O_FSYNC instead of O_SYNC */
+#define O_SYNC O_FSYNC
+#endif
+
+#if defined(__FreeBSD__)
+/* FreeBSD uses these alternate names for fields in the sigval structure */
+#define sival_int sigval_int
+#define sival_ptr sigval_ptr
+#endif
+
 #endif
