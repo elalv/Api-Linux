e2eb7f17936d774ec75f2fadb3b93bb9b5ed553c
elalv <admin@elalv.xyz>
2019-01-21 10:21:15 +0300

    [3.3][UPDATE] Diagnose 'errno' error by outputError

--- lib/error_functions.c
+++ lib/error_functions.c
@@ -27,3 +27,42 @@ terminate(Boolean useExit3)
     else
         _exit(EXIT_FAILURE);
 }
+
+/* Diagnose 'errno' error by:
+
+      * outputting a string containing the error name (if available
+        in 'ename' array) corresponding to the value in 'err', along
+        with the corresponding error message from strerror(), and
+
+      * outputting the caller-supplied error message specified in
+        'format' and 'ap'. */
+
+static void
+outputError(Boolean useErr, int err, Boolean flushStdout,
+            const char *format, va_list ap)
+{
+#define BUF_SIZE 500
+    char buf[BUF_SIZE], userMsg[BUF_SIZE], errText[BUF_SIZE];
+
+    vsnprintf(userMsg, BUF_SIZE, format, ap);
+
+    if (useErr)
+        snprintf(errText, BUF_SIZE, " [%s %s]",
+                 (err > 0 && err <= MAX_ENAME) ? ename[err] : "?UNKNOWN?", strerror(err));
+    else
+        snprintf(errText, BUF_SIZE, ":");
+
+#if __GNUC__ >= 7
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation"
+#endif
+    snprintf(buf, BUF_SIZE, "ERROR%s %s\n", errText, userMsg);
+#if __GNUC__ >= 7
+#pragma GCC diagnostic pop
+#endif
+
+    if (flushStdout)
+        fflush(stdout); /* Flush any pending stdout */
+    fputs(buf, stderr);
+    fflush(stderr); /* In case stderr is not line-buffered */
+}
