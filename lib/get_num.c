d2f85b42fe75bfca085543d961de41a9f53f3f2a
elalv <admin@elalv.xyz>
2019-02-08 11:22:55 +0300

    [3.6][UPDATE] gnFail:  Print a diagnostic msg

--- lib/get_num.c
+++ lib/get_num.c
@@ -2,3 +2,22 @@
 
    Functions to process numeric command-line arguments.
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Print a diagnostic message that contains a function name ('fname'),
+   the value of a command-line argument ('arg'), the name of that
+   command-line argument ('name'), and a diagnostic error message ('msg'). */
+
+static void
+gnFail(const char *fname, const char *msg, const char *arg, const char *name)
+{
+    fprintf(stderr, "%s error", fname);
+    if (name != NULL)
+        fprintf(stderr, " (in %s)", name);
+    fprintf(stderr, ": %s\n", msg);
+    if (arg != NULL && *arg != '\0')
+        fprintf(stderr, "        offending text: %s\n", arg);
+
+    exit(EXIT_FAILURE);
+}
