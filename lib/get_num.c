3816f3a0cad81798e5350b933e839c2ee5cdd712
elalv <admin@elalv.xyz>
2019-02-11 11:33:45 +0300

    [3.6][UPDATE] getNum: Convert a numeric command-line argument into a long integer

--- lib/get_num.c
+++ lib/get_num.c
@@ -4,6 +4,8 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include "get_num.h"
 
 /* Print a diagnostic message that contains a function name ('fname'),
    the value of a command-line argument ('arg'), the name of that
@@ -20,4 +22,44 @@ gnFail(const char *fname, const char *msg, const char *arg, const char *name)
         fprintf(stderr, "        offending text: %s\n", arg);
 
     exit(EXIT_FAILURE);
+}
+
+/* Convert a numeric command-line argument ('arg') into a long integer,
+   returned as the function result. 'flags' is a bit mask of flags controlling
+   how the conversion is done and what diagnostic checks are performed on the
+   numeric result; see get_num.h for details.
+
+   'fname' is the name of our caller, and 'name' is the name associated with
+   the command-line argument 'arg'. 'fname' and 'name' are used to print a
+   diagnostic message in case an error is detected when processing 'arg'. */
+
+static long
+getNum(const char *fname, const char *arg, int flags, const char *name)
+{
+    long res;
+    char *endptr;
+    int base;
+
+    if (arg == NULL || *arg == '\0')
+        gnFail(fname, "null or empty string", arg, name);
+
+    base = (flags & GN_ANY_BASE) ? 0 : (flags & GN_BASE_8) ? 8
+                                   : (flags & GN_BASE_16)  ? 16
+                                                           : 10;
+
+    errno = 0;
+    res = strtol(arg, &endptr, base);
+    if (errno != 0)
+        gnFail(fname, "strtol() failed", arg, name);
+
+    if (*endptr != '\0')
+        gnFail(fname, "nonnumeric characters", arg, name);
+
+    if ((flags & GN_NONNEG) && res < 0)
+        gnFail(fname, "negative value not allowed", arg, name);
+
+    if ((flags & GN_GT_0) && res <= 0)
+        gnFail(fname, "value must be > 0", arg, name);
+
+    return res;
 }
