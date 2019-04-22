d97d5e17dab58547730327b1cbbeaa80da655b66
elalv <admin@elalv.xyz>
2019-04-22 15:12:41 +0300

    [8.1][UPDATE] userNameFromId:   Return name corresponding to 'uid', or NULL on error

--- users_groups/ugid_functions.c
+++ users_groups/ugid_functions.c
@@ -3,3 +3,14 @@
    Implements a set of functions that convert user/group names to user/group IDs
    and vice versa.
 */
+#include <pwd.h>
+#include "ugid_functions.h" /* Declares functions defined here */
+
+char * /* Return name corresponding to 'uid', or NULL on error */
+userNameFromId(uid_t uid)
+{
+    struct passwd *pwd;
+
+    pwd = getpwuid(uid);
+    return (pwd == NULL) ? NULL : pwd->pw_name;
+}
--- users_groups/ugid_functions.h
+++ users_groups/ugid_functions.h
@@ -2,3 +2,11 @@
 
    Header file for ugid_functions.c.
 */
+#ifndef UGID_FUNCTIONS_H
+#define UGID_FUNCTIONS_H
+
+#include "tlpi_hdr.h"
+
+char *userNameFromId(uid_t uid);
+
+#endif
