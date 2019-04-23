2afb1080a09dd4957216f3900377fb7a0ba6b55b
elalv <admin@elalv.xyz>
2019-04-23 15:14:33 +0300

    [8.1][UPDATE] userIdFromName: Return UID corresponding to 'name', or -1 on error

--- users_groups/ugid_functions.c
+++ users_groups/ugid_functions.c
@@ -14,3 +14,24 @@ userNameFromId(uid_t uid)
     pwd = getpwuid(uid);
     return (pwd == NULL) ? NULL : pwd->pw_name;
 }
+
+uid_t /* Return UID corresponding to 'name', or -1 on error */
+userIdFromName(const char *name)
+{
+    struct passwd *pwd;
+    uid_t u;
+    char *endptr;
+
+    if (name == NULL || *name == '\0') /* On NULL or empty string */
+        return -1;                     /* return an error */
+
+    u = strtol(name, &endptr, 10); /* As a convenience to caller */
+    if (*endptr == '\0')           /* allow a numeric string */
+        return u;
+
+    pwd = getpwnam(name);
+    if (pwd == NULL)
+        return -1;
+
+    return pwd->pw_uid;
+}
