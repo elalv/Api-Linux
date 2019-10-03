360f3f295ed562929f57f11775f48ebe109ecb0c
elalv <admin@elalv.xyz>
2019-10-03 20:32:46 +0300

    [23.6][UPDATE] itimerspecFromStr: Convert a string of the following form to an itimerspec structure

--- timers/itimerspec_from_str.h
+++ timers/itimerspec_from_str.h
@@ -2,3 +2,11 @@
 
    Header file for itimerspec_from_str.c.
 */
+#ifndef ITIMERSPEC_FROM_STR_H
+#define ITIMERSPEC_FROM_STR_H
+
+#include <time.h>
+
+void itimerspecFromStr(char *str, struct itimerspec *tsp);
+
+#endif
