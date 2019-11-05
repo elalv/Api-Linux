30b4fa772f58add17b99a77946b05c64a1fc066f
elalv <admin@elalv.xyz>
2019-11-05 03:41:57 +0300

    [26.2][UPDATE] printWaitStatus: Dissect and print the process termination status

--- procexec/print_wait_status.h
+++ procexec/print_wait_status.h
@@ -2,3 +2,9 @@
 
    Header file for print_wait_status.c.
 */
+#ifndef PRINT_WAIT_STATUS_H /* Prevent accidental double inclusion */
+#define PRINT_WAIT_STATUS_H
+
+void printWaitStatus(const char *msg, int status);
+
+#endif
