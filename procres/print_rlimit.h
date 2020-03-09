39391bcdbb37fe191b24a2b134cc5235f10c6c8b
elalv <admin@elalv.xyz>
2020-03-09 23:40:53 +0300

    [36.2][UPDATE] Displaying process resource limits

--- procres/print_rlimit.h
+++ procres/print_rlimit.h
@@ -2,3 +2,9 @@
 
    Header file for print_rlimit.c.
 */
+#ifndef PRINT_RLIMIT_H /* Prevent accidental double inclusion */
+#define PRINT_RLIMIT_H
+
+int printRlimit(const char *msg, int resource);
+
+#endif
