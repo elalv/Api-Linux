aee109880e33ed17d8e8b5efb6dcbfb8574e3f85
elalv <admin@elalv.xyz>
2020-03-11 07:32:45 +0300

    [36.2A][UPDATE] Print the contents of an 'rusage' (resource usage) structure

--- procres/print_rusage.h
+++ procres/print_rusage.h
@@ -2,3 +2,11 @@
 
    Header file for print_rusage.c.
 */
+#ifndef PRINT_RUSAGE_H /* Prevent accidental double inclusion */
+#define PRINT_RUSAGE_H
+
+#include <sys/resource.h>
+
+void printRusage(const char *leader, const struct rusage *ru);
+
+#endif
