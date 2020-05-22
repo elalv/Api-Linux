e63a351d70a39739c330b4782ad50a680705a292
elalv <admin@elalv.xyz>
2020-05-22 07:23:05 +0300

    [47.10][UPDATE] initSemAvailable: Initialize semaphore to 1 (i.e., "available")

---
+++ svsem/binary_sems.c
@@ -0,0 +1,17 @@
+/* binary_sems.c
+
+   Implement a binary semaphore protocol using System V semaphores.
+*/
+#include <sys/types.h>
+#include <sys/sem.h>
+#include "semun.h" /* Definition of semun union */
+#include "binary_sems.h"
+
+int /* Initialize semaphore to 1 (i.e., "available") */
+initSemAvailable(int semId, int semNum)
+{
+    union semun arg;
+
+    arg.val = 1;
+    return semctl(semId, semNum, SETVAL, arg);
+}
--- svsem/binary_sems.h
+++ svsem/binary_sems.h
@@ -2,3 +2,11 @@
 
    Header file for binary_sems.c.
 */
+#ifndef BINARY_SEMS_H /* Prevent accidental double inclusion */
+#define BINARY_SEMS_H
+
+#include "tlpi_hdr.h"
+
+int initSemAvailable(int semId, int semNum);
+
+#endif
