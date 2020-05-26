5a0be1d6da24cc3ec663ec27c714d6e582c50792
elalv <admin@elalv.xyz>
2020-05-26 07:27:43 +0300

    [47.10][UPDATE] reserveSem: Reserve semaphore - decrement it by 1

--- svsem/binary_sems.h
+++ svsem/binary_sems.h
@@ -7,8 +7,15 @@
 
 #include "tlpi_hdr.h"
 
+/* Variables controlling operation of functions below */
+
+extern Boolean bsUseSemUndo;   /* Use SEM_UNDO during semop()? */
+extern Boolean bsRetryOnEintr; /* Retry if semop() interrupted by signal handler? */
+
 int initSemAvailable(int semId, int semNum);
 
 int initSemInUse(int semId, int semNum);
+
+int reserveSem(int semId, int semNum);
 
 #endif
