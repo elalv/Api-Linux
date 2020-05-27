ba1fa90941d9f163c1cfb781ec3225c1fcf26011
elalv <admin@elalv.xyz>
2020-05-27 05:36:10 +0300

    [47.10][UPDATE] releaseSem: Release semaphore - increment it by 1

--- svsem/binary_sems.c
+++ svsem/binary_sems.c
@@ -46,3 +46,15 @@ reserveSem(int semId, int semNum)
 
     return 0;
 }
+
+int /* Release semaphore - increment it by 1 */
+releaseSem(int semId, int semNum)
+{
+    struct sembuf sops;
+
+    sops.sem_num = semNum;
+    sops.sem_op = 1;
+    sops.sem_flg = bsUseSemUndo ? SEM_UNDO : 0;
+
+    return semop(semId, &sops, 1);
+}
--- svsem/binary_sems.h
+++ svsem/binary_sems.h
@@ -18,4 +18,6 @@ int initSemInUse(int semId, int semNum);
 
 int reserveSem(int semId, int semNum);
 
+int releaseSem(int semId, int semNum);
+
 #endif
