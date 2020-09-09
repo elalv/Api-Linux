6d755bea7f5b50630ba5053b30bfee8e80fa0d4c
elalv <admin@elalv.xyz>
2020-09-09 03:24:39 +0300

    [55.3][UPDATE] lockRegion: Lock a file region using nonblocking F_SETLK

---
+++ filelock/region_locking.c
@@ -0,0 +1,27 @@
+/* region_locking.c
+
+   Some useful functions for file region (fcntl()) locking.
+*/
+#include <fcntl.h>
+#include "region_locking.h" /* Declares functions defined here */
+
+/* Lock a file region (private; public interfaces below) */
+
+static int
+lockReg(int fd, int cmd, int type, int whence, int start, off_t len)
+{
+    struct flock fl;
+
+    fl.l_type = type;
+    fl.l_whence = whence;
+    fl.l_start = start;
+    fl.l_len = len;
+
+    return fcntl(fd, cmd, &fl);
+}
+
+int /* Lock a file region using nonblocking F_SETLK */
+lockRegion(int fd, int type, int whence, int start, int len)
+{
+    return lockReg(fd, F_SETLK, type, whence, start, len);
+}
--- filelock/region_locking.h
+++ filelock/region_locking.h
@@ -2,3 +2,11 @@
 
    Header file for region_locking.c.
 */
+#ifndef REGION_LOCKING_H
+#define REGION_LOCKING_H
+
+#include <sys/types.h>
+
+int lockRegion(int fd, int type, int whence, int start, int len);
+
+#endif
