9578dd73567d160cd758caae10091cd3a7c3b0f4
elalv <admin@elalv.xyz>
2020-09-11 07:39:43 +0300

    [55.4][UPDATE] Creating a PID lock file to ensure just one instance of a program is started

--- filelock/create_pid_file.h
+++ filelock/create_pid_file.h
@@ -2,3 +2,11 @@
 
    Header file for create_pid_file.c.
 */
+#ifndef CREATE_PID_FILE_H /* Prevent accidental double inclusion */
+#define CREATE_PID_FILE_H
+
+#define CPF_CLOEXEC 1
+
+int createPidFile(const char *progName, const char *pidFile, int flags);
+
+#endif
