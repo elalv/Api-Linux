1ef9a94a3b40a70d5b980feaf91537a97ad0831f
elalv <admin@elalv.xyz>
2020-10-30 16:45:00 +0300

    [61.1][UPDATE] writen: Write 'n' bytes to 'fd' from 'buf'

--- sockets/rdwrn.c
+++ sockets/rdwrn.c
@@ -35,3 +35,34 @@ readn(int fd, void *buffer, size_t n)
     }
     return totRead; /* Must be 'n' bytes if we get here */
 }
+
+/* Write 'n' bytes to 'fd' from 'buf', restarting after partial
+   write or interruptions by a signal handlers */
+
+ssize_t
+writen(int fd, const void *buffer, size_t n)
+{
+    ssize_t numWritten; /* # of bytes written by last write() */
+    size_t totWritten;  /* Total # of bytes written so far */
+    const char *buf;
+
+    buf = buffer; /* No pointer arithmetic on "void *" */
+    for (totWritten = 0; totWritten < n;)
+    {
+        numWritten = write(fd, buf, n - totWritten);
+
+        /* The "write() returns 0" case should never happen, but the
+           following ensures that we don't loop forever if it does */
+
+        if (numWritten <= 0)
+        {
+            if (numWritten == -1 && errno == EINTR)
+                continue; /* Interrupted --> restart write() */
+            else
+                return -1; /* Some other error */
+        }
+        totWritten += numWritten;
+        buf += numWritten;
+    }
+    return totWritten; /* Must be 'n' bytes if we get here */
+}
--- sockets/rdwrn.h
+++ sockets/rdwrn.h
@@ -9,4 +9,6 @@
 
 ssize_t readn(int fd, void *buf, size_t len);
 
+ssize_t writen(int fd, const void *buf, size_t len);
+
 #endif
