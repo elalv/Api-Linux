1bcabad9d69fea26b8c6da4a9b95755a12b5a737
elalv <admin@elalv.xyz>
2020-11-10 16:58:47 +0300

    [61.A][UPDATE] Header file used by scm_cred_send.c and scm_cred_recv.c

---
+++ sockets/scm_cred.h
@@ -0,0 +1,12 @@
+/* scm_cred.h
+
+   Header file used by scm_cred_send.c and scm_cred_recv.c.
+*/
+#define _GNU_SOURCE /* To get SCM_CREDENTIALS definition from <sys/socket.h> */
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdbool.h>
+#include "unix_sockets.h" /* Declares our socket functions */
+#include "tlpi_hdr.h"
+
+#define SOCK_PATH "scm_cred"
