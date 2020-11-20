f24c4040878d8cea1361ce5c7bc067a23b27d886
elalv <admin@elalv.xyz>
2020-11-20 20:04:44 +0300

    [61.A][UPDATE] Header file used by scm_rights_send.c and scm_rights_recv.c

---
+++ sockets/scm_rights.h
@@ -0,0 +1,12 @@
+/* scm_rights.h
+
+   Header file used by scm_rights_send.c and scm_rights_recv.c.
+*/
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include "unix_sockets.h" /* Declares our unix*() socket functions */
+#include "tlpi_hdr.h"
+
+#define SOCK_PATH "scm_rights"
