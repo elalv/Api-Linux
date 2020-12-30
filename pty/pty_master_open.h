5a13357a605f1d47bd9aba2e286dad872b1e8d05
elalv <admin@elalv.xyz>
2020-12-30 18:19:17 +0300

    [64.1][UPDATE] ptyMasterOpen: Open a pty master

--- pty/pty_master_open.h
+++ pty/pty_master_open.h
@@ -2,3 +2,11 @@
 
    Header file for pty_open.c (and pty_master_open_bsd.c).
 */
+#ifndef PTY_MASTER_OPEN_H
+#define PTY_MASTER_OPEN_H
+
+#include <sys/types.h>
+
+int ptyMasterOpen(char *slaveName, size_t snLen);
+
+#endif
