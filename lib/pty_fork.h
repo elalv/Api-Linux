635b95cd92362a005c8049407b34e67b3006a9a5
elalv <admin@elalv.xyz>
2021-01-08 16:22:02 +0300

    [LIB][UPDATE] Pseudoterminals

---
+++ lib/pty_fork.h
@@ -0,0 +1,15 @@
+/* pty_fork.h
+
+   Header file for pty_fork.c.
+*/
+#ifndef FORK_PTY_H
+#define FORK_PTY_H
+
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+
+pid_t ptyFork(int *masterFd, char *slaveName, size_t snLen,
+              const struct termios *slaveTermios, const struct winsize *slaveWS);
+
+#endif
